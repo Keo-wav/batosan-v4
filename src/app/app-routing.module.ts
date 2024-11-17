28a1a66152517ad47f857c4a331498f1be165b23
Nicolas NACKPHOUMINH <n.nackphouminh@gmail.com>
2024-11-17 14:36:04 +0100

    STYLE - some style tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Ex1Component } from './features/ex1/ex1.component';
 import { WordsComponent } from './features/words/words.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'words', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'ex1', component: Ex1Component },
   { path: 'words', component: WordsComponent },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
